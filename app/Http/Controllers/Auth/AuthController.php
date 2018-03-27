d368f40b887c338d6decc6b5b850f212848494d7
s3544333 <s3544333@student.rmit.edu.au>
2018-03-27 21:53:07 +1100

    Password Validation
    Add regex and error message

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -51,9 +51,11 @@ class AuthController extends Controller
         return Validator::make($data, [
             'name' => 'required|max:255',
             'email' => 'required|email|max:255|unique:users',
-            'password' => 'required|min:6|confirmed',
+            'password' => 'required|min:6|confirmed|regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/',
         ]);
     }
+
+
 
     /**
      * Create a new user instance after a valid registration.
