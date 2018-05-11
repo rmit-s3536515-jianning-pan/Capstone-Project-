6d820d5b0a23658d4631582badcb8c5a74aa6a26
Mai <s3544333@student.rmit.edu.au>
2018-05-11 13:30:33 +1000

    Added Test Cases, and some Design Templates

---
+++ resources/views/_eventContainer.blade.php
@@ -0,0 +1,84 @@
+      <div class="col-sm-6 col-md-4 my-2" data-toggle="modal" data-target="#myModal">
+        <div class="shadow-1 blue-hover" style="background-color: white">
+          <div class="post post__grid">
+            <div class="post__media">
+              <div class="images">
+                <div class="images__fit">
+                <img src="https://images.pexels.com/photos/442559/pexels-photo-442559.jpeg?auto=compress&cs=tinysrgb" alt="">
+               </div>
+             </div>
+            <div class="post__date">
+                <span class="day">29</span>
+                <span class="month">march</span>
+            </div>
+          </div>
+        </div>
+            
+        <div class="post__entry">
+            <p>Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla sagittis sollicitudin nisl consectetur posuere...</p>
+        </div>
+      </div>
+    </div>
+
+          <div class="col-sm-6 col-md-4 my-2" data-toggle="modal" data-target="#myModal">
+        <div class="shadow-1 blue-hover" style="background-color: white">
+          <div class="post post__grid">
+            <div class="post__media">
+              <div class="images">
+                <div class="images__fit">
+                <img src="images/m.jpg" alt="">
+              </div>
+               </div>
+            <div class="post__date">
+                <span class="day">29</span>
+                <span class="month">march</span>
+            </div>
+          </div>
+        </div>
+            
+        <div class="post__entry">
+            <p>Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla sagittis sollicitudin nisl consectetur posuere...</p>
+        </div>
+      </div>
+    </div>
+
+      <div class="col-sm-6 col-md-4 my-2" data-toggle="modal" data-target="#myModal">
+        <div class="shadow-1 blue-hover" style="background-color: white">
+          <div class="post post__grid">
+            <div class="post__media">
+              <div class="images">
+                <div class="images__fit">
+                <img src="images/q.jpg" alt="">
+               </div>
+             </div>
+            <div class="post__date">
+                <span class="day">29</span>
+                <span class="month">march</span>
+            </div>
+          </div>
+        </div>
+            
+        <div class="post__entry">
+            <p>Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla sagittis sollicitudin nisl consectetur posuere...</p>
+        </div>
+      </div>
+    </div>
+
+  <!-- Modal -->
+<!--   <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog"> -->
+    
+      <!-- Modal content-->
+<!--       <div class="modal-content mod text-center modal-dialog">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Interested in this event? <br> Sign-Up to access this cool Event</h4>
+        </div>
+        <div class="modal-footer text-center">
+          <button type="button" class="btn btn-default start-btn" data-dismiss="modal">Close</button>
+          <a href="{{ url('/register') }}"><button type="button" class="btn btn-default start-btn" data-dismiss="modal">Sign-Up</button></a>
+        </div>
+      </div>
+      
+    </div>
+  </div> -->
---
+++ resources/views/_homepage_search.blade.php
@@ -0,0 +1,54 @@
+<!-- search box start -->
+    <div class="container-fluid search search-bar ">
+        <form class="col-md-8 col-md-offset-2" method="get" action="{{ url('event/showall') }}">
+            <div class="row">
+                <div class="col-md-5 margin-t-b">
+                    <label class="invisible" for="keywords">Name</label>
+                    <div class="input-group input-group-lg">
+                        <input type="text" name="keywords" class="form-control" placeholder="What are you looking for?">
+                    </div>
+                </div>
+                <div class="col-md-5 margin-t-b">
+                    <label class="invisible" >Classification</label>
+                    <div class="input-group input-group-lg">
+                        <select class="selectpicker" multiple="multiple" name="subs[]"  data-selected-text-format="count" data-actions-box="true"
+                        data-size="8" title="All Categories">
+                            @foreach ($categories as $cate)
+                                <optgroup label="{{ $cate->cat_name }}" title="{{ $cate->cat_name}}" > 
+                                     @foreach ($subs as $sub)
+                                                @if ($sub->cate_id==$cate->id)   
+                                            <option value="{{ $sub->id}}">
+                                                {{ $sub->name}}
+                                            </option>>
+                                               
+                                                @endif
+                                    @endforeach
+                                </optgroup> 
+                                   <!--  <label class="check">{{ $cate->cat_name}}
+                                     <input type="checkbox" name="categories[]" value="{{ $cate->id}}">
+                                         <span class="checkmark"></span>
+                                      </label> -->
+                                   
+                            @endforeach
+                        </select>   
+
+                       
+                            
+                        
+                    </div>
+                </div>
+
+
+                <div class="col-md-2 margin-t-b">
+                    <label class="invisible">hidden</label>
+                    <div class="input-group input-group-lg">
+                        <button type="submit" class="btn btn-success btn-lg start-btn" style="width:100%">Search</button>
+                    </div>
+                    
+                </div>
+            </div>
+
+            </div>
+        </form>
+    </div>
+    <!-- search box ends -->
